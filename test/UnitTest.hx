0d30b2b43cbbbfe341710e31daf5e5a96ba6a8cd
Michael Baczynski <michael@polygonal.de>
2013-01-27 03:19:57 +0100

    add UnitTest

--- test/UnitTest.hx
+++ test/UnitTest.hx
@@ -7,44 +7,51 @@ class UnitTest extends haxe.unit.TestRunner
 		new UnitTest();
 	}
 	
-	public function new()
+	function new()
 	{
 		super();
 		
 		#if alchemy
-			#if (flash10 || cpp)
-			add(new test.mem.TestMemoryManager());
-			run();
-			this.cases = new List<haxe.unit.TestCase>();
-			de.polygonal.ds.mem.MemoryManager.free();
-			de.polygonal.ds.mem.MemoryManager.RESERVE_BYTES = 1024 * 1024 * 20;
-			de.polygonal.ds.mem.MemoryManager.BLOCK_SIZE_BYTES = 1024 * 512;
-			#end
+		#if (flash10 || cpp)
+		add(new test.mem.TestMemoryManager());
+		run();
+		this.cases = new List<haxe.unit.TestCase>();
+		de.polygonal.ds.mem.MemoryManager.free();
+		de.polygonal.ds.mem.MemoryManager.RESERVE_BYTES = 1024 * 1024 * 20;
+		de.polygonal.ds.mem.MemoryManager.BLOCK_SIZE_BYTES = 1024 * 512;
+		#end
 		#end
 		
-		add(new TestArray2());
-		add(new TestArray3());
+		//cpp crash with dce-no, cpp + generic fails compiling
+		
+		add(new TestArray2());			//cpp and --no-inline: testRemove() fails
+		add(new TestArray3());			//cpp and --no-inline: testRemove() fails
 		add(new TestArrayedDeque());
 		add(new TestArrayedQueue());
 		add(new TestArrayedStack());
 		add(new TestArrayUtil());
+		
 		add(new TestBinaryTree());
-		add(new TestBits());
+		
+		add(new TestBits());			//neko & haxe3 fails
 		add(new TestBitVector());
 		add(new TestBST());
 		add(new TestDA());
 		add(new TestDLL());
 		add(new TestDLLCircular());
 		add(new TestGraph());
+		
 		#if flash9
 		add(new TestHashMap());
 		#end
+		
 		add(new TestHashSet());
 		add(new TestHashTable());
 		add(new TestHeap());
 		add(new TestIntHashSet());
 		add(new TestIntHashTable());
 		add(new TestIntIntHashTable());
+		
 		add(new TestLinkedDeque());
 		add(new TestLinkedQueue());
 		add(new TestLinkedStack());
@@ -54,7 +61,7 @@ class UnitTest extends haxe.unit.TestRunner
 		add(new TestTree());
 		
 		add(new test.mem.TestByteMemory());
-		add(new test.mem.TestBitMemory());
+		add(new test.mem.TestBitMemory());	//neko & haxe3 fails
 		add(new test.mem.TestShortMemory());
 		add(new test.mem.TestFloatMemory());
 		add(new test.mem.TestDoubleMemory());
