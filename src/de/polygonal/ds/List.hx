8ccdb07ac91f76e022d69c32bc0ae119a46eab1d
Michael Baczynski <michaelbaczynski@gmail.com>
2016-02-25 15:34:51 +0100

    Added List<T> interface

---
+++ src/de/polygonal/ds/List.hx
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2008-2016 Michael Baczynski, http://www.polygonal.de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package de.polygonal.ds;
+
+import de.polygonal.ds.Collection;
+
+interface List<T> extends Collection<T>
+{
+	/**
+		Returns the value at the given `index`.
+	**/
+	function get(index:Int):T;
+	
+	/**
+		Overwrites the value at the given `index` with `val`.
+	**/
+	function set(index:Int, val:T):Void;
+	
+	/**
+		Adds `val` to the end of the list.
+	**/
+	function add(val:T):Void;
+	
+	/**
+		Inserts an element at the specified index. hint shift, if index == size??
+	**/
+	function insert(index:Int, val:T):Void;
+	
+	/**
+		Removes the value at the given `index`. hint shift
+	**/
+	function removeAt(index:Int):T;
+	
+	/**
+		Returns the index of the first occurrence of `val`, or -1 if this list does not contain `val`.
+	**/
+	function indexOf(val:T):Int;
+	
+	/**
+		Returns a shallow copy of a range of elements in the interval [`fromIndex`, `toIndex`).
+		If `toIndex` is negative, the value represents the number of elements.
+	**/
+	function getRange(fromIndex:Int, toIndex:Int):List<T>;
+}
