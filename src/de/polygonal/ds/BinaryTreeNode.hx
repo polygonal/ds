5b24aab037f580d39d87965da4f4cc53a523e3b5
Benjamin Van Treese <vantreeseba@gmail.com>
2016-04-11 21:55:20 -0500

    Correct comment for isLeaf

--- src/de/polygonal/ds/BinaryTreeNode.hx
+++ src/de/polygonal/ds/BinaryTreeNode.hx
@@ -479,7 +479,7 @@ class BinaryTreeNode<T> implements Collection<T>
 	}
 	
 	/**
-		Returns true if this node is a root node (``p`` is null).
+		Returns true if this node is a leaf node (``l`` and ``r`` are null).
 	**/
 	public inline function isLeaf():Bool
 	{
