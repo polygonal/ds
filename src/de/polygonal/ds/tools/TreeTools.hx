517c828c8e2f677e2b3f6339e5731d2d82645292
Michael Baczynski <michaelbaczynski@gmail.com>
2016-05-03 16:50:33 +0200

    Minor

--- src/de/polygonal/ds/tools/TreeTools.hx
+++ src/de/polygonal/ds/tools/TreeTools.hx
@@ -18,8 +18,6 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 package de.polygonal.ds.tools;
 
-import haxe.ds.StringMap;
-
 /**
 	A helper class for working with trees.
 **/
