f97dd6302eaaae2ae0651887c7b2bb7b76a85ca0
Michael Baczynski <michaelbaczynski@gmail.com>
2019-02-01 21:31:32 +0100

    Simplify Assert class

--- src/polygonal/ds/tools/Assert.hx
+++ src/polygonal/ds/tools/Assert.hx
@@ -18,89 +18,45 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 package polygonal.ds.tools;
 
-#if macro
-import haxe.macro.Context;
-import haxe.macro.Expr;
-#end
-
 /**
-	Assertion macro injecting assertion statements
-	
 	An assertion specifies a condition that you expect to be true at a point in your program.
 **/
 class Assert
 {
-	#if runtime_assert
-		#if debug
-		public static function assert(predicateExpr:Bool, ?msg:String)
+	#if debug
+	#if verbose_assert
+	macro public static inline function assert(predicateExpr:haxe.macro.Expr, ?message:String)
+	{
+		var predicate  = new haxe.macro.Printer().printExpr(predicateExpr);
+		var p          = haxe.macro.Context.currentPos();
+		var location   = haxe.macro.PositionTools.toLocation(p);
+		var methodName = haxe.macro.Context.getLocalMethod();
+		var className  = haxe.macro.Context.getLocalClass().toString();
+		var infos      = macro {fileName: $v{location.file}, lineNumber: $v{location.range.start.line}, className: $v{className}, methodName: $v{methodName}};
+		if (message != null) predicate = '$message ($predicate)';
+		return macro untyped polygonal.ds.tools.Assert._assert($e{predicateExpr}, $v{predicate}, $e{infos});
+	}
+	static inline function _assert(predicate:Bool, ?message:String, ?pos:haxe.PosInfos)
+	#else
+	public static inline function assert(predicate:Bool, ?message:String, ?pos:haxe.PosInfos)
+	#end
+	{
+		if (!predicate)
 		{
 			#if js
-			js.Browser.console.assert(predicateExpr, msg);
+			throw new js.Error(format(message, pos));
 			#else
-			if (!predicateExpr) throw 'Assertion failed: $msg';
+			throw format(message, pos);
 			#end
 		}
-		#else
-		@:extern public static inline function assert(predicateExpr:Bool, ?msg:String) {}
-		#end
-	#else
-	macro public static function assert(predicateExpr:Expr, ?msgExpr:Expr):Expr
-	{
-		if (Context.defined("display")) return macro {};
-		if (!Context.defined("debug")) return macro {};
-		
-		switch (Context.typeof(predicateExpr))
-		{
-			case TAbstract(_, _):
-			case _: Context.error("`predicateExpr` should be a boolean", predicateExpr.pos);
-		}
-		
-		var p = Context.currentPos();
-		var location = haxe.macro.PositionTools.toLocation(p);
-		var locationInfos = " (in file " +  location.file + ", line " + location.range.start.line + ")";
-		
-		var extra = false;
-		var error = false;
-		switch (Context.typeof(msgExpr))
-		{
-			case TMono(t):
-				error = t.get() != null;
-			
-			case TInst(t, _):
-				error = t.get().name != "String";
-				extra = true;
-			
-			case _:
-				error = true;
-		}
-		if (error) Context.error("`msgExpr` should be a string", msgExpr.pos);
-		
-		var predicate = new haxe.macro.Printer().printExpr(predicateExpr);
-		
-		var infos =
-		if (extra)
-			macro ${msgExpr} + $v{" [" + predicate + "]"} + $v{locationInfos};
-		else
-			macro $v{predicate} + $v{locationInfos};
-		
-		return
-		if (Context.defined("js"))
-		{
-			{expr: ECall(
-				{pos: p, expr: EField(macro $p{["js", "Syntax"]}, "code")},
-				[
-					macro $v{"console.assert({0}, {1})"},
-					predicateExpr,
-					infos
-				]), pos: p};
-		}
-		else
-		{
-			{expr: EIf(
-				{expr: EBinop(OpNotEq, macro $i{"true"}, predicateExpr), pos: p},
-				{expr: EThrow(macro $v{"Assertion failed:"} + ${infos}), pos: p}, null),
-				pos: p};
-		}
 	}
+	static function format(message:String, pos:haxe.PosInfos)
+	{
+		var locationInfos = 'in file ${pos.fileName}, line ${pos.lineNumber}';
+		var s = message == null ? locationInfos : '$message ($locationInfos)';
+		return 'Assertion failed' + (message != null ? ": " : " ") + s;
+	}
+	#else
+	macro public static inline function assert(predicate:haxe.macro.Expr, rest:Array<haxe.macro.Expr>) return macro {}
 	#end
 }
